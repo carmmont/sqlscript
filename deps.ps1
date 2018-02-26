415b9a388e0d381922724ed57479b81e186c80f2
aseduto <aseduto@hotmail.com>
2018-02-26 12:01:57 +0100

    adding appveyor

---
+++ deps.ps1
@@ -0,0 +1,15 @@
+
+
+$url = 'https://github.com/Microsoft/sqltoolsservice/blob/master/bin/nuget/Microsoft.SqlServer.Smo.140.2.9.nupkg';
+
+if(-Not (test-path './bin/nuget'))
+{
+     [System.IO.Directory]::CreateDirectory('./bin/nuget');
+}
+
+if(-Not (test-path './bin/nuget/Microsoft.SqlServer.Smo.140.2.9.nupkg'))
+{
+
+    Invoke-WebRequest $url -OutFile './bin/nuget/Microsoft.SqlServer.Smo.140.2.9.nupkg';
+
+}
