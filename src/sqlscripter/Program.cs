5a558026c95bc0fdcfe5683561a651210240ca2b
aseduto <aseduto@hotmail.com>
2018-02-26 16:53:56 +0100

    adding simple tests

--- src/sqlscripter/Program.cs
+++ src/sqlscripter/Program.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Reflection;
+using System.Linq;
+using System.Data;
 
 namespace sqlscripter
 {
@@ -49,8 +51,34 @@ namespace sqlscripter
             Console.BackgroundColor = ConsoleColor.Black;
             Console.Write(progress.ToString() + " of " + total.ToString() + "    "); //blanks at the end remove any excess
         }
-        static void add_urns_from_collection(SchemaCollectionBase coll, UrnCollection urns)
+
+        static void add_urn_from_query(Database db, string obj, Func<string, string, string> geturn, UrnCollection urns, bool progress = true)
         {
+            var dt = DateTime.UtcNow;
+            Console.WriteLine("PROCESSING {0}", obj);
+
+            DataSet ds = db.ExecuteWithResults(@"select s.name as [schema], o.name from sys.objects o
+                    inner join sys.schemas s
+                    on o.schema_id = s.schema_id where type = '" + obj + "'");
+
+            int count = ds.Tables[0].Rows.Count;
+            int idx = 1;
+           
+            foreach (DataRow r in ds.Tables[0].Rows)
+            {
+                    if(progress)
+                        drawTextProgressBar(idx++, count);
+                //urns.Add(db.StoredProcedures[r[1].ToString(), r[0].ToString()].Urn);
+                urns.Add(geturn(r[1].ToString(), r[0].ToString()));
+                        
+            }
+
+            Console.WriteLine("PROCESSED {0} {1}ms", obj, DateTime.UtcNow.Subtract(dt).TotalMilliseconds);
+        }
+
+        static void add_urns_from_collection(SchemaCollectionBase coll, UrnCollection urns, bool progress = true)
+        {
+            var dt = DateTime.UtcNow;
             Console.WriteLine("PROCESSING {0}", coll.GetType().Name);
 
             int count = coll.Count;
@@ -58,20 +86,31 @@ namespace sqlscripter
 
             foreach (SqlSmoObject ob in coll)
             {
-                drawTextProgressBar(idx++, count);
+                if(progress)
+                    drawTextProgressBar(idx++, count);
 
-                PropertyInfo isSystem = ob.GetType().GetProperty("IsSystemObject");
-
-                if (null == isSystem
-                    || (!(bool)isSystem.GetValue(ob))
-                    )
+                if (ob is StoredProcedure)
                 {
-                    urns.Add(ob.Urn);
+                    if (!((StoredProcedure)ob).IsSystemObject)
+                    {
+                        urns.Add(ob.Urn);
+                    }
+                }
+                else
+                {
+                    PropertyInfo isSystem = ob.GetType().GetProperty("IsSystemObject");
+
+                    if (null == isSystem
+                        || (!(bool)isSystem.GetValue(ob))
+                        )
+                    {
+                        urns.Add(ob.Urn);
+                    }
                 }
  
             }
 
-            Console.WriteLine("PROCESSED {0}", coll.GetType().Name);
+            Console.WriteLine("PROCESSED {0} {1}ms", coll.GetType().Name, DateTime.UtcNow.Subtract(dt).TotalMilliseconds);
         }
 
         static string NormalizeUrn(string urn)
@@ -119,16 +158,19 @@ namespace sqlscripter
             var sqluser = commandLineApplication.Option("-U | --user", "Sql User", CommandOptionType.SingleValue);
             var sqlpsw = commandLineApplication.Option("-P | --psw", "Sql Password", CommandOptionType.SingleValue);
             var sqldb  = commandLineApplication.Option("-d | --database", "Sql Database", CommandOptionType.SingleValue);
-            
+            var useprogress = commandLineApplication.Option("--no-progress", "Disable progress bar", CommandOptionType.NoValue);
+
             
             commandLineApplication.Command("dbindex", command =>
             {
                 command.Options.AddRange(command.Parent.Options);
 
                 var indexfile = command.Option("-i | --index", "Generate Index File", CommandOptionType.SingleValue);
-
+                var querymode = command.Option("--query-mode", "Use object query for objects", CommandOptionType.NoValue);
+                
                 command.OnExecute( () =>
                 {                   
+                    DateTime pinned = DateTime.UtcNow;
 
                     ServerConnection serverConnection = new ServerConnection(sqlserver.Value(), sqluser.Value(), sqlpsw.Value());
                     Server server = new Server(serverConnection);
@@ -138,32 +180,51 @@ namespace sqlscripter
                     {
                          AllowSystemObjects = false
                          , WithDependencies = true
-                         
-                
+
                     };
 
                     scripter.Options = op;
 
                     UrnCollection urns = new UrnCollection();
 
-                    Console.WriteLine("CONNECTED");
+                    Console.WriteLine("CONNECTED ({0}ms)", DateTime.UtcNow.Subtract(pinned).TotalMilliseconds);
+                    pinned = DateTime.UtcNow;
+
+                    bool fast = querymode.HasValue();
 
                     //server.GetSmoObject
+
+                    Database db = server.Databases[sqldb.Value()];
                      
-                    TableCollection tc = server.Databases[sqldb.Value()].Tables;
+                    TableCollection tc = db.Tables;
 
-                    add_urns_from_collection(tc, urns);            
+                    add_urns_from_collection(tc, urns, (!useprogress.HasValue()));
 
-                    var sp = server.Databases[sqldb.Value()].StoredProcedures;
-                                        
-                    //StoredProcedure s;
-                    //s.IsSystemObject
+                    if (fast)
+                    {
+                        add_urn_from_query(db, "P", (sp, sch) => db.StoredProcedures[sp, sch].Urn, urns, (!useprogress.HasValue()));
+                    }
+                    else
+                    {
 
-                    add_urns_from_collection(sp, urns);     
-                     
-                    var vs = server.Databases[sqldb.Value()].Views;
+                        var sp = server.Databases[sqldb.Value()].StoredProcedures;
+                        add_urns_from_collection(sp, urns);
+                    }
 
-                    add_urns_from_collection(vs, urns);    
+                    //--------------------------------
+
+
+                    if (fast)
+                    {
+                        add_urn_from_query(db, "V", (sp, sch) => db.Views[sp, sch].Urn, urns, (!useprogress.HasValue()));
+                    }
+                    else
+                    {
+
+                        var vs = server.Databases[sqldb.Value()].Views;
+
+                        add_urns_from_collection(vs, urns);
+                    }
                     
                     var ss = server.Databases[sqldb.Value()].Synonyms;
 
@@ -177,16 +238,26 @@ namespace sqlscripter
 
                     add_urns_from_collection(tt, urns); 
 
-                    Console.WriteLine("DISCOVERING");
+                    Console.WriteLine("DISCOVERING ({0}ms)", DateTime.UtcNow.Subtract(pinned).TotalMilliseconds);
+                    pinned = DateTime.UtcNow;
 
                     //scripter.DiscoveryProgress += Scripter_DiscoveryProgress;
                     DependencyTree tr = scripter.DiscoverDependencies(urns, true);
 
-                    Console.WriteLine("DEPENDENCY");
+                    Console.WriteLine("DEPENDENCY ({0}ms)", DateTime.UtcNow.Subtract(pinned).TotalMilliseconds);
+                    pinned = DateTime.UtcNow;
 
                     DependencyCollection dc = scripter.WalkDependencies(tr);
 
                     string path = indexfile.Value();
+
+                    if (null != path)
+                    {
+                        if(System.IO.File.Exists(path))
+                            System.IO.File.Delete(path);
+
+                        System.IO.File.AppendAllText(path, "#file auto-generated" + Environment.NewLine);
+                    }
                     
                     foreach (DependencyCollectionNode j in dc)
                     {
@@ -197,7 +268,8 @@ namespace sqlscripter
 
                         if (null != path)
                         {
-                            System.IO.File.AppendAllText(path, NormalizeUrn(j.Urn));
+                            System.IO.File.AppendAllText(path, NormalizeUrn(j.Urn) + Environment.NewLine);
+                            //System.IO.File.AppendAllText(path,  );
                         }
                         else
                         {
@@ -266,7 +338,7 @@ namespace sqlscripter
                     string outputdir = output.Value() ?? "./";
                                            
                                         
-                   Script(sqldb, objs, server, scripter, outputdir);
+                   Script(objs, server.Databases[sqldb.Value()], scripter, outputdir, (!useprogress.HasValue()));
 
                     
                 });
@@ -374,106 +446,131 @@ namespace sqlscripter
         }
         */
 
-        private static void Script(CommandOption sqldb, string[] target, Server server
-        , Scripter scripter, string output)
+        private static void Script(string[] target, Database db
+        , Scripter scripter, string output, bool progress)
         {
             SqlSmoObject[] objs = new SqlSmoObject[1];
           
-
             int count = target.Length;
             int jdx = 1;
             
+            
             foreach (string obname in target)
             {
-            
                 obj_info oi = ObjectInfo(obname);
 
                 scripter.Options.IncludeIfNotExists = true;
                 scripter.Options.ScriptForCreateDrop = false;
                 
-
-                if ("Table" == oi.type)
+                if ("null" == oi.type || "comment" == oi.type)
                 {
-                    objs[0] = server.Databases[sqldb.Value()].Tables[oi.name, oi.schema];
-                    
-
+                    continue;
                 }
-
-                if ("StoredProcedure" == oi.type)
-                {
-                    objs[0] = server.Databases[sqldb.Value()].StoredProcedures[oi.name, oi.schema];
-                    
-
-                    scripter.Options.IncludeIfNotExists = false;
-                    scripter.Options.ScriptForCreateDrop = true;
-                }
-
-                if ("View" == oi.type)
-                {
-                    objs[0] = server.Databases[sqldb.Value()].Views[oi.name, oi.schema];
-                    
-
-                    scripter.Options.IncludeIfNotExists = false;
-                    scripter.Options.ScriptForCreateDrop = true;
-                }
-
-                if ("Synonym" == oi.type)
-                {
-                   objs[0] = server.Databases[sqldb.Value()].Synonyms[oi.name, oi.schema];
-                    
-                }
-
-                if ("UserDefinedFunction" == oi.type)
-                {
-                    objs[0] = server.Databases[sqldb.Value()].UserDefinedFunctions[oi.name, oi.schema];
-                    
-
-                    scripter.Options.IncludeIfNotExists = false;
-                    scripter.Options.ScriptForCreateDrop = true;
-                }
-
-                if ("UserDefinedType" == oi.type)
-                {
-                    objs[0] = server.Databases[sqldb.Value()].UserDefinedTypes[oi.name, oi.schema];
-                    
-                }
-
-                //DependencyTree tr = scripter.DiscoverDependencies(objs, true);
-                //DependencyCollection dc = scripter.WalkDependencies(tr);
-
-                StringCollection sqls = scripter.Script(objs);
 
                 string file = null;
 
                 if (null != output)
                 {
                     file = FilePath(output, oi);
-                    drawTextProgressBar(jdx++, count);
-
-                    if(System.IO.File.Exists(file))
+               
+                    if (System.IO.File.Exists(file))
                         System.IO.File.Delete(file);
                 }
 
-                for (int idx = 0; idx < sqls.Count; idx++)
-                {
-                    string sql = sqls[idx];
 
-                    if (file == output)
-                    {
-                        Console.WriteLine("GO");
-                        //Console.WriteLine($"------- START {prefix} {idx} [{oi.schema}].[{oi.name}]-------");
-                        Console.WriteLine(sql);
-                        //Console.WriteLine($"------- END {prefix} {idx} [{oi.schema}].[{oi.name}] -------");
-                    }
-                    else
-                    {
-                        System.IO.File.AppendAllText(file,  Environment.NewLine +  "GO" + Environment.NewLine);
-                        System.IO.File.AppendAllText(file, sql);
-                    }
+                if ("Table" == oi.type)
+                {
+                    objs[0] = db.Tables[oi.name, oi.schema];
+
                 }
+
+                if ("StoredProcedure" == oi.type)
+                {
+                    objs[0] = db.StoredProcedures[oi.name, oi.schema];
+                                        
+                    ScriptDrop(scripter, objs, file);
+                }
+
+                if ("View" == oi.type)
+                {
+                    objs[0] = db.Views[oi.name, oi.schema];
+                    
+                    ScriptDrop(scripter, objs, file);
+                }
+
+                if ("Synonym" == oi.type)
+                {
+                    objs[0] = db.Synonyms[oi.name, oi.schema];
+
+                }
+
+                if ("UserDefinedFunction" == oi.type)
+                {
+                    objs[0] = db.UserDefinedFunctions[oi.name, oi.schema];
+
+                    ScriptDrop(scripter, objs, file);
+                    
+                }
+
+                if ("UserDefinedType" == oi.type)
+                {
+                    objs[0] = db.UserDefinedTypes[oi.name, oi.schema];
+
+                }
+
+                //DependencyTree tr = scripter.DiscoverDependencies(objs, true);
+                //DependencyCollection dc = scripter.WalkDependencies(tr);
+
+                if (null != output && progress)
+                {                    
+                    drawTextProgressBar(jdx++, count);
+                }
+
+                Script(scripter, objs, file);
 
             }
 
+        }
+
+        private static void ScriptDrop(Scripter scripter, SqlSmoObject[] objs, string file)
+        {
+            scripter.Options.ScriptForCreateDrop = true;
+            scripter.Options.ScriptDrops = true;
+
+                    Script(scripter, objs, file);
+                    
+            scripter.Options.IncludeIfNotExists = false;
+            scripter.Options.ScriptDrops = false;
+        }
+
+        private static void Script(Scripter scripter, SqlSmoObject[] objs, string file)
+        {
+            StringCollection sqls = scripter.Script(objs);
+            
+            for (int idx = 0; idx < sqls.Count; idx++)
+            {
+                string sql = sqls[idx];
+
+                if(sql == "SET ANSI_NULLS ON" 
+                    || sql == "SET QUOTED_IDENTIFIER ON"
+                    )
+                    continue;
+
+                if (file == null)
+                {
+                    Console.WriteLine("GO");
+                    //Console.WriteLine($"------- START {prefix} {idx} [{oi.schema}].[{oi.name}]-------");
+                    Console.WriteLine(sql);
+                    //Console.WriteLine($"------- END {prefix} {idx} [{oi.schema}].[{oi.name}] -------");
+                }
+                else
+                {
+                    System.IO.File.AppendAllText(file, Environment.NewLine + "GO" + Environment.NewLine);
+                    System.IO.File.AppendAllText(file, sql);
+                }
+            }
+
+            
         }
 
         private static string FilePath(string output, obj_info oi, bool dooutput = true)
