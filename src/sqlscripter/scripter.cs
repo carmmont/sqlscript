da8b969013d9df3b6ca64e67f5428e86a4e11a04
aseduto <aseduto@hotmail.com>
2019-02-05 18:07:25 +0100

    WIP. Versioning

--- src/sqlscripter/scripter.cs
+++ src/sqlscripter/scripter.cs
@@ -3,11 +3,17 @@ using Microsoft.SqlServer.Management.Common;
 using Microsoft.SqlServer.Management.Smo;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Data;
 
 namespace sqlscripter{
 class scripter {
 
     private static bool _do_version = false;
+    private static string EXTENDED_PROPERTY_TEMPLATE = @"EXEC sys.sp_addextendedproperty @name=N'$NAME', @value=N'$VALUE' , @level0type=N'SCHEMA',@level0name=N'$SCHEMA', @level1type=N'$TYPE',@level1name=N'$TARGET';
+    GO
+    ";
+    private static string FILE_VERSION = "FILE VERSION";
+    private static string DATABASE_VERSION = "DATABASE VERSION";
     private static string fixdefault(string sql, string defaultrx)
     {
         var m = System.Text.RegularExpressions.Regex.Match(sql, defaultrx);
@@ -15,55 +21,195 @@ class scripter {
         string defaultsql = $"ADD CONSTRAINT {m.Groups[2].Value} DEFAULT";
 
         return sql.Replace("ADD  DEFAULT", defaultsql);
+    }
+
+    private static string extended_type(obj_info oi)
+    {
+        string type = null;
+
+        if ("Table" == oi.type)
+        {
+            type = "TABLE";
+        }
+
+        if ("StoredProcedure" == oi.type)
+        {
+            type = "PROCEDURE";
+        }
+
+        if ("View" == oi.type)
+        {
+            type = "VIEW";
+        }
+
+        if ("Synonym" == oi.type)
+        {
+           type = "SYNONYM"; 
+        }
+
+        if ("UserDefinedFunction" == oi.type)
+        {
+            type = "FUNCTION";
+        }
+
+        if ("UserDefinedType" == oi.type)
+        {
+            type = "TYPE";
+        }
+
+        return type;
 
     }
-        private static void Script(Scripter scripter
-            , SqlSmoObject[] objs
-            , string file)
+
+    private static string compile_extended_template(string property
+        , string value
+        , string schema
+        , string type
+        , string target
+    )
+    {
+        string extended = EXTENDED_PROPERTY_TEMPLATE.Replace("$NAME", property);
+       extended = extended.Replace("$VALUE", value);
+       extended = extended.Replace("$SCHEMA", schema);
+       extended = extended.Replace("$TYPE", type);
+       extended = extended.Replace("$TARGET", target);
+       
+       return extended;
+    }
+
+    private static string get_extended_rx(string property)
+    {
+       string rx = "([^']+)";
+       
+       string extended = compile_extended_template(property, rx, rx, rx, rx);
+              
+       return extended;
+    }
+
+    private static string get_extended_value(string property, string sql)
+    {
+        var m = System.Text.RegularExpressions.Regex.Match(sql, get_extended_rx(property));
+
+        if(m.Groups.Count > 1)
+            return m.Groups[1].Value;
+
+        return null;
+    }
+
+    private static string get_extended_version(string sql)
+    {
+        return get_extended_value(FILE_VERSION, sql);
+    }
+
+    private static string clean_up_version(string sql)
+    {
+        
+
+        sql = System.Text.RegularExpressions.Regex.Replace(sql, get_extended_rx(FILE_VERSION), "");
+        sql = System.Text.RegularExpressions.Regex.Replace(sql, get_extended_rx(DATABASE_VERSION), "");
+
+        return sql;
+    }
+
+    private static bool is_version_valid(string version)
+    {
+        return System.Text.RegularExpressions.Regex.IsMatch(version, @"\d+\.\d+\.\d+\.\d+");
+    }
+
+    private static string Script(Scripter scripter
+        , SqlSmoObject[] objs
+        , string file = null
+        , string prefix = ""
+        , string version = null
+        , obj_info oi = null)
+    {
+        StringCollection sqls = scripter.Script(objs);
+
+        string sql_return = "";
+        
+        for (int idx = 0; idx < sqls.Count; idx++)
         {
-            StringCollection sqls = scripter.Script(objs);
-            
-            for (int idx = 0; idx < sqls.Count; idx++)
+            string sql = sqls[idx];
+
+            if(sql == "SET ANSI_NULLS ON" 
+                || sql == "SET QUOTED_IDENTIFIER ON"
+                )
+                continue;
+
+            string defaultrx = @"IF NOT EXISTS \(SELECT \* FROM sys.objects WHERE object_id = OBJECT_ID\(N'\[([^\]]+)\]\.\[([^\]]+)\]'\) AND type = 'D'\)";
+
+            if (System.Text.RegularExpressions.Regex.IsMatch(sql, defaultrx))
             {
-                string sql = sqls[idx];
-
-                if(sql == "SET ANSI_NULLS ON" 
-                    || sql == "SET QUOTED_IDENTIFIER ON"
-                    )
-                    continue;
-
-                string defaultrx = @"IF NOT EXISTS \(SELECT \* FROM sys.objects WHERE object_id = OBJECT_ID\(N'\[([^\]]+)\]\.\[([^\]]+)\]'\) AND type = 'D'\)";
-
-                if (System.Text.RegularExpressions.Regex.IsMatch(sql, defaultrx))
-                {
-                    sql = fixdefault(sql, defaultrx);
-                }
-
-                if (file == null)
-                {
-                    Console.WriteLine("GO");
-                    //Console.WriteLine($"------- START {prefix} {idx} [{oi.schema}].[{oi.name}]-------");
-                    Console.WriteLine(sql);
-                    //Console.WriteLine($"------- END {prefix} {idx} [{oi.schema}].[{oi.name}] -------");
-                }
-                else
-                {
-                    System.IO.File.AppendAllText(file, Environment.NewLine + "GO" + Environment.NewLine);
-                    System.IO.File.AppendAllText(file, sql);
-                }
+                sql = fixdefault(sql, defaultrx);
             }
 
+            sql = prefix + sql;
+            sql += Environment.NewLine + "GO" + Environment.NewLine;
+
+            sql_return = sql_return + sql;
+
+            if(null != version)
+            {
+                if(!is_version_valid(version))
+                    throw new ScripterException("invalid version " + version);
+
+                if(null == oi)
+                    throw new ScripterException("version must specify oi");
+
+                if(file != null)
+                {                    
+                    string baseline = "----";
+                    if(System.IO.File.Exists(file))
+                    {
+                        baseline = System.IO.File.ReadAllText(file);
+                        baseline = clean_up_version(baseline);
+                    }
+
+                    if(baseline != sql)
+                    {
+                        string[ ] semver = version.Split(".");
+                        semver[2] = (Int16.Parse(semver[2]) + 1).ToString();
+
+                        version = $"{semver[0]}.{semver[1]}.{semver[2]}.0";
+                    }
+                }
+
+                string type = extended_type(oi);
+                if(null != type)
+                {
+                    sql += compile_extended_template(FILE_VERSION, version, oi.schema, type , oi.name);
+                    sql += compile_extended_template(DATABASE_VERSION, "0.0.0.0", oi.schema, type, oi.name);
+                }
+            }
             
+
+            if (file != null)
+            {
+                if(System.IO.File.Exists(file))
+                {                       
+                   System.IO.File.Delete(file);
+                }
+
+                //System.IO.File.AppendAllText(file, Environment.NewLine + "GO" + Environment.NewLine);
+                System.IO.File.AppendAllText(file, sql);
+            }
         }
-    private static void ScriptDrop(Scripter scripter, SqlSmoObject[] objs, string file)
+
+        return sql_return;
+
+        
+    }
+    private static string ScriptDrop(Scripter scripter, SqlSmoObject[] objs)
         {
             scripter.Options.ScriptForCreateDrop = true;
             scripter.Options.ScriptDrops = true;
 
-                    Script(scripter, objs, file);
+                    string sql = Script(scripter, objs);
                     
             scripter.Options.IncludeIfNotExists = false;
             scripter.Options.ScriptDrops = false;
+
+            return sql;
         }
 
     private static void check_oi(SqlSmoObject obj, obj_info oi)
@@ -73,105 +219,151 @@ class scripter {
                     string.Format("cannot find {0}: {2} {1}", oi.type, oi.name, oi.schema)
                 );
         }
+
+    private static string get_object_version(Database db, obj_info oi)
+    {
+        //AGGREGATE, DEFAULT, FUNCTION, LOGICAL FILE NAME, PROCEDURE, QUEUE
+        //, RULE, SEQUENCE, SYNONYM, TABLE, TABLE_TYPE, TYPE, VIEW, XML SCHEMA COLLECTION, and NULL.
+        //db.ExecuteWithResults
+
+        string type = extended_type(oi);
+
+        
+        if(null != type)
+        {
+            string sql = $"SELECT value FROM fn_listextendedproperty('{FILE_VERSION}', 'schema', 'dbo', '{type}', NULL, NULL, NULL)";
+            DataSet ds = db.ExecuteWithResults(sql);
+
+            if(ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+            {
+                return ds.Tables[0].Rows[0][0].ToString();
+            }
+        }
+
+        return null;
+    }
     
     private static void Script(string[] target, Database db
         , Scripter scripter, string output, bool progress)
-        {
-            SqlSmoObject[] objs = new SqlSmoObject[1];
-          
-            int count = target.Length;
-            int jdx = 0;
-                        
-            foreach (string obname in target)
-            {
-                obj_info oi = util.ObjectInfo(obname);
-
-                scripter.Options.IncludeIfNotExists = true;
-                scripter.Options.ScriptForCreateDrop = false;
-                
-                if ("null" == oi.type || "comment" == oi.type)
-                {
-                    jdx++;
-                    continue;
-                }
-
-                string file = null;
-
-                if (null != output)
-                {
-                    file = util.FilePath(output, oi);
-               
-                    if (System.IO.File.Exists(file))
-                        System.IO.File.Delete(file);
-                }
-
-
-                if ("Table" == oi.type)
-                {
-                    scripter.Options.DriDefaults = true;
-                    objs[0] = db.Tables[oi.name, oi.schema];
-                    check_oi(objs[0], oi);
-                }
-
-                if ("StoredProcedure" == oi.type)
-                {
-                    objs[0] = db.StoredProcedures[oi.name, oi.schema];
-                    check_oi(objs[0], oi);                    
-                    ScriptDrop(scripter, objs, file);
-                }
-
-                if ("View" == oi.type)
-                {
-                    objs[0] = db.Views[oi.name, oi.schema];
-                    check_oi(objs[0], oi);
-                    ScriptDrop(scripter, objs, file);
-                }
-
-                if ("Synonym" == oi.type)
-                {
-                    objs[0] = db.Synonyms[oi.name, oi.schema];
-                    check_oi(objs[0], oi);
-                    ScriptDrop(scripter, objs, file);
-
-                }
-
-                if ("UserDefinedFunction" == oi.type)
-                {
-                    objs[0] = db.UserDefinedFunctions[oi.name, oi.schema];
-                    check_oi(objs[0], oi);
-                    ScriptDrop(scripter, objs, file);
+    {
+        SqlSmoObject[] objs = new SqlSmoObject[1];
+        
+        int count = target.Length;
+        int jdx = 0;
                     
-                }
+        foreach (string obname in target)
+        {
+            obj_info oi = util.ObjectInfo(obname);
 
-                if ("UserDefinedType" == oi.type)
+            scripter.Options.IncludeIfNotExists = true;
+            scripter.Options.ScriptForCreateDrop = false;
+            
+            if ("null" == oi.type || "comment" == oi.type)
+            {
+                jdx++;
+                continue;
+            }
+
+            string file = null;
+
+            string prefix = "";
+
+            
+            if (null != output)
+            {
+                file = util.FilePath(output, oi);
+            
+                //if (System.IO.File.Exists(file))
+                    //System.IO.File.Delete(file);
+            }
+
+            string version = null;
+            if(_do_version)
+            {
+                version = get_object_version(db, oi);
+
+                if(null == version)
+                    version = "0.0.0.0";
+
+                if(null != file && System.IO.File.Exists(file))
                 {
-                    objs[0] = db.UserDefinedTypes[oi.name, oi.schema];
-                    check_oi(objs[0], oi);
-                }
+                    string sql_file = System.IO.File.ReadAllText(file);
+                    string file_version = get_extended_version(sql_file);
 
-                if ("Schema" == oi.type)
-                {
-                    objs[0] = db.Schemas[oi.name];
-                    check_oi(objs[0], oi);
+                    if( null != file_version && 0 < version.CompareTo(file_version))
+                    {
+                        version = file_version;
+                    }
                 }
-                                
-                if(null == objs[0])
-                {
-                    throw new ScripterException(string.Format("Invalid type: {0} {1}", oi.type, obname));
-                }
-                //DependencyTree tr = scripter.DiscoverDependencies(objs, true);
-                //DependencyCollection dc = scripter.WalkDependencies(tr)
+            }
 
-                if (null != output && progress)
-                {                    
-                    util.drawTextProgressBar(++jdx, count, obname);
-                }
 
-                Script(scripter, objs, file);
+            if ("Table" == oi.type)
+            {
+                scripter.Options.DriDefaults = true;
+                objs[0] = db.Tables[oi.name, oi.schema];
+                check_oi(objs[0], oi);
+            }
+
+            if ("StoredProcedure" == oi.type)
+            {
+                objs[0] = db.StoredProcedures[oi.name, oi.schema];
+                check_oi(objs[0], oi);                    
+                prefix = ScriptDrop(scripter, objs);
+            }
+
+            if ("View" == oi.type)
+            {
+                objs[0] = db.Views[oi.name, oi.schema];
+                check_oi(objs[0], oi);
+                prefix = ScriptDrop(scripter, objs);
+            }
+
+            if ("Synonym" == oi.type)
+            {
+                objs[0] = db.Synonyms[oi.name, oi.schema];
+                check_oi(objs[0], oi);
+                prefix = ScriptDrop(scripter, objs);
 
             }
 
+            if ("UserDefinedFunction" == oi.type)
+            {
+                objs[0] = db.UserDefinedFunctions[oi.name, oi.schema];
+                check_oi(objs[0], oi);
+                prefix = ScriptDrop(scripter, objs);
+                
+            }
+
+            if ("UserDefinedType" == oi.type)
+            {
+                objs[0] = db.UserDefinedTypes[oi.name, oi.schema];
+                check_oi(objs[0], oi);
+            }
+
+            if ("Schema" == oi.type)
+            {
+                objs[0] = db.Schemas[oi.name];
+                check_oi(objs[0], oi);
+            }
+                            
+            if(null == objs[0])
+            {
+                throw new ScripterException(string.Format("Invalid type: {0} {1}", oi.type, obname));
+            }
+            //DependencyTree tr = scripter.DiscoverDependencies(objs, true);
+            //DependencyCollection dc = scripter.WalkDependencies(tr)
+
+            if (null != output && progress)
+            {                    
+                util.drawTextProgressBar(++jdx, count, obname);
+            }
+
+            Script(scripter, objs, file, prefix, version);
+
         }
+
+    }
 
     public static void Script(string[] target, Database db
         , string output, bool progress, SqlServerVersion sql_version
--- test/create.sql
+++ test/create.sql
@@ -1,5 +1,3 @@
-
-GO
 IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'Gathering')
 EXEC sys.sp_executesql N'CREATE SCHEMA [Gathering]'
 
@@ -31,45 +29,6 @@ CREATE TABLE [dbo].[TABLE1](
 	[NAME] [nvarchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 END
-GO
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Gathering].[Exec_Query_Plans_1]') AND type in (N'U'))
-BEGIN
-CREATE TABLE [Gathering].[Exec_Query_Plans_1](
-	[Id] [bigint] IDENTITY(1,1) NOT NULL,
-	[plan_handle] [varbinary](64) NOT NULL,
-	[plan_generation_num] [bigint] NOT NULL,
-	[statement_start_offset] [int] NOT NULL,
-	[statement_end_offset] [int] NOT NULL,
-	[dbid] [smallint] NULL,
-	[objectId] [int] NULL,
-	[query_plan] [nvarchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[creation_time] [datetime] NULL,
-	[correlation_id] [uniqueidentifier] NOT NULL,
-	[timestamp] [datetime] NOT NULL,
- CONSTRAINT [PK_Exec_Query_Plans_11] PRIMARY KEY NONCLUSTERED 
-(
-	[plan_handle] ASC,
-	[plan_generation_num] ASC,
-	[statement_start_offset] ASC,
-	[statement_end_offset] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-END
-GO
-IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[Gathering].[Exec_Query_Plans_1]') AND name = N'IX_Exec_Query_Plans_11')
-CREATE UNIQUE CLUSTERED INDEX [IX_Exec_Query_Plans_11] ON [Gathering].[Exec_Query_Plans_1]
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-GO
-SET ANSI_PADDING ON
-
-GO
-IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[Gathering].[Exec_Query_Plans_1]') AND name = N'IX_Exec_Query_Plans_1')
-CREATE NONCLUSTERED INDEX [IX_Exec_Query_Plans_1] ON [Gathering].[Exec_Query_Plans_1]
-(
-	[plan_handle] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Gathering].[DF__Exec_Quer__times__37A5467C]') AND type = 'D')
 BEGIN
@@ -415,3 +374,4 @@ IF  EXISTS (SELECT * FROM sys.synonyms WHERE name = N'TABLE2' AND schema_id = SC
 DROP SYNONYM [dbo].[TABLE2]
 GO
 CREATE SYNONYM [dbo].[TABLE2] FOR [TESTDB].[dbo].[TABLE1]
+GO
